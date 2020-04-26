04455694ae0759169eac9d095acf149f38790e88
Alexander Suvorov <Bambofet@mail.ru>
2020-04-26 14:49:35 +0300

    Interfaces and classes for Domain.Core

---
+++ Microservices/Microservices.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microservices.Domain.Core.Events;
+
+namespace Microservices.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
---
+++ Microservices/Microservices.Domain.Core/Commands/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microservices.Domain.Core.Events;
+
+namespace Microservices.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
